026b2b2b23672c8d2ffdd9308361c0a742f2fda4
angelinaBazzi <angibazzi@gmail.com>
2022-04-28 19:09:21 -0300

    pruebassss
    poker ad

--- 2.1.py
+++ 2.1.py
@@ -2,8 +2,12 @@ import statistics
 import matplotlib.pyplot as plt
 from os import system
 from random import randint
+import math
+import statistics
+
 
 def generador_GCL():
+<<<<<<< Updated upstream
     numeros = []
     m = (2**31)-1 # modulo
     a = 7**5  # multiplicador
@@ -16,10 +20,28 @@ def generador_GCL():
         numeros.append(nro)
     print(numeros)
     return numeros
+=======
+        global numeros
+        m = 2**48 #modulo
+        a = 25214903917  #multipliador
+        c = 11 #Incremento
+        semilla = 14758
+        numeros.append(semilla)
+
+        for i in range(1,100):
+            nro = (a*numeros[i-1] + c) % m
+            numeros.append(nro)
+        print("GCL:     ",numeros)
+>>>>>>> Stashed changes
 
 def generador_pmc():
+    global numeros
     seed = 6923
+<<<<<<< Updated upstream
     n = 100  # cantidad de numeros generados
+=======
+    n=100#cantidad de numeros generados
+>>>>>>> Stashed changes
     numeros = [seed]
     sem = []
     sem2 = []
@@ -43,7 +65,16 @@ def completa_ceros(sem):
         sem = '0' + sem
     return sem
 
+def test_rachas():
+    media = statistics.median(numeros)
+    Z = abs(prueba_de_rachas(numeros, media))
+    print(Z)
+    if (Z > 1.96): #Para un nivel de confianza del 95% --> Zcritico = 0.025
+        print("Los numeros no son aleatorios ")
+    else:
+        print("Los numeros son aleatorios ")
 
+<<<<<<< Updated upstream
 def aboveandbelow():
     n1 = []  # Numeros por encima de la media
     n2 = []  # Numeros por debajo de la media
@@ -66,3 +97,43 @@ def aboveandbelow():
 
 # generador_pmc()
 aboveandbelow()
+=======
+def prueba_de_rachas(numeros, media):
+    rachas, n1, n2 = 0, 0, 0
+
+    
+    for i in range(len(numeros)):
+
+        if (numeros[i] >= media and numeros[i - 1] < media) or \
+                (numeros[i] < media and numeros[i - 1] >= media):
+            rachas += 1
+
+        if (numeros[i]) >= media:
+            n1 += 1
+        else:
+            n2 += 1
+    rachas_esp = ((2 * n1 * n2) / (n1 + n2)) + 1 #Número de corridas esperadas
+    desv = math.sqrt((2 * n1 * n2 * (2 * n1 * n2 - n1 - n2)) / (((n1 + n2) ** 2) * (n1 + n2 - 1))) # Desviación estándar del número de carreras
+
+    z = (rachas - rachas_esp) / desv
+
+    return z
+numero = "55344"
+poker = []
+c = 0
+
+for i in numero:
+    compara = i
+    for j in range(0,5):
+        if compara == numero[j]:
+            c+= 1
+        print ('j= ', j, ' compara: ', compara, numero[j])
+    poker.append(c)
+    c = 0
+print (poker)
+
+numeros=[]
+#generador_pmc()
+generador_GCL()
+test_rachas()
+>>>>>>> Stashed changes
